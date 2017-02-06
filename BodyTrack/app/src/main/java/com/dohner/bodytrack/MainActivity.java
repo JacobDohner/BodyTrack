cf8ef402150ad5c787e19118b08eb55cc751b9d2
Jacob Dohner <dohner7@gmail.com>
2017-02-06 12:18:13 -0500

    Adding saving date and weight

--- BodyTrack/app/src/main/java/com/dohner/bodytrack/MainActivity.java
+++ BodyTrack/app/src/main/java/com/dohner/bodytrack/MainActivity.java
@@ -3,17 +3,33 @@ package com.dohner.bodytrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.view.View;
 import android.view.View.OnClickListener;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.lang.Object;
 
 public class MainActivity extends AppCompatActivity {
 
     Button saveCurrentWeight;
+    ArrayList currentWeightTrackingList;
+    ArrayList currentWeightDateList;
+    EditText currentWeightText;
+    Calendar cal;
+    String dateBunch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        currentWeightTrackingList = new ArrayList<>();
+        currentWeightDateList = new ArrayList<>();
+        currentWeightText = (EditText) findViewById(R.id.currentWeightText);
+        cal = Calendar.getInstance();
+        dateBunch = new String();
 
         addListenerOnButton();
     }
@@ -27,8 +43,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View arg0) {
 
+                if(currentWeightText!=null) {
+                currentWeightTrackingList.add(saveCurrentWeight);
+                dateBunch = Integer.toString(cal.MONTH) + "." + Integer.toString(cal.DAY_OF_MONTH) + "." + Integer.toString(cal.YEAR);
+                currentWeightDateList.add(dateBunch);
+                }
+                }
 
-            }
+
 
         });
 
